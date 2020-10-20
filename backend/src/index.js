8709ccd166bd6da270fbe3c7f3a0d8fb4ba4af9b
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2020-10-19 22:11:46 -0300

    Studyng params.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,54 @@
-const { request } = require('express');
+const { request, response } = require('express');
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
+app.use(express.json());
+
+//temporary db
+const projects = [];
+
 app.get('/projects', (request, response)=>{
-    return response.json({message:'Hello World'}) ;
+    /* const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner); */
+
+    //aula 8 - 04:34
+
+    return response.json(projects) ;
 });
+
+app.post('/projects', (request, response)=>{
+    const {title, owner} = request.body;
+
+    const project = {id: uuid(), title, owner};
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response)=>{
+    const params = request.params;
+
+    const project = projects.find(project => project.id === id)
+
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response)=>{
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
 
 app.listen(3333, ()=> {
     console.log('🚀 Back-end started!');
